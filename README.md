d805f70d140e789ed59aa814e85f21ab1b4a41a9
Ben <ben3670@live.com.au>
2022-06-19 12:32:44 +1000

    develop

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dictionary Results Web App
+
+* Dictionary web app made for <A href="https://www.udemy.com/course/100-days-of-code/">100 days of code on Udemey.</A>
+* Uses the <a href='https://owlbot.info/'>Owlbot</a> dictionary api.  
+
+</img>
+<img src = "docs/demo.gif", alt = "demo gif", height = "350">
---
+++ main.py
@@ -0,0 +1,38 @@
+import os 
+
+import requests
+from requests.exceptions import HTTPError
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+def get_api_dictionary_response(word) -> dict:
+    base_url= 'https://owlbot.info/api/v4/dictionary/'
+
+    headers={
+    'Authorization': os.getenv('OWL_BOT_AUTH')
+    }
+
+    url = f'{base_url}{word}'
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+@app.route("/", methods=['GET', 'POST']) 
+def home():
+    # if request.method == 'POST':
+    word = request.form.get('word', 'dog').strip().lower()
+    # try: 
+    try: 
+        data = get_api_dictionary_response(word)
+    except HTTPError:
+        data = None
+    return render_template("index.html", data=data, word=word)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
